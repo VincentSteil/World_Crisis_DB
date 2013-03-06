2e3cbfb4cd93536c53a395c159a423aef52480b2
Carlos <Carlos.balderas@utexas.edu>
2013-03-06 02:48:07 -0600

    setup for Running acceptance tests; changed structure of WCDB1.py

---
+++ RunWCDB1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""
+To run the program
+    % python RunWCDB1.py < RunWCDB1.in.xml > RunWCDB1.out.xml
+    % chmod ugo+x RunCollatz.py
+    % RunCollatz.py < RunCollatz.in > RunCollatz.out
+
+To document the program
+    % pydoc -w WCDB1
+"""
+
+# -------
+# imports
+# -------
+
+import sys
+from WCDB1 import WCDB1_run
+
+WCDB1_run
--- WCDB1.py
+++ WCDB1.py
@@ -6,7 +6,20 @@ Lab 3
 import sys
 import xml.etree.ElementTree as ET
 from cStringIO import StringIO
-strr = sys.stdin.read()
-tree = ET.parse(StringIO(strr)) #importing the XML
-root = tree.getroot()
-print ET.tostring(root) #printing the result to the console
+
+
+def WCBD_print(w, tree):
+  w.write(tree)
+def WCDB1_run(r ,w):
+  """
+  r is a reader
+  w is a writer
+  """
+  #strr = sys.stdin.read()
+  strr = r
+  tree = ET.parse(StringIO(strr)) #importing the XML
+  root = tree.getroot()
+  
+  #print ET.tostring(root)
+  
+  WCDB_print(w, ET.tostring(root)) #printing the result to the console
