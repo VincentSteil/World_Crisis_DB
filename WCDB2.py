7478ddedcd45fdc284e86dee5049d425cd355e08
mduban <iwo@utexas.edu>
2013-04-02 02:01:25 -0500

    Lab4 starting

---
+++ WCDB2.py
@@ -0,0 +1,45 @@
+# -------------------
+# WCDB2.py
+# Group name: Is Yuchen Here Today?
+# Lab 4
+# -------------------
+
+# -------------
+# imports
+# -------------
+
+import sys
+import xml.etree.ElementTree as ET
+from cStringIO import StringIO
+import _mysql
+
+# -------------
+# WCDB_print
+# -------------
+
+def WCDB2_print(w, tree):
+  """
+  This function prints tree to writer w
+  w is a writer
+  tree is a string to print
+  """
+  w.write(tree) #printing to .out file
+
+# -------------
+# WCDB2_run
+# -------------
+
+  
+def WCDB2_run(r ,w):
+  """
+  This function reads from the input and prints to input an XML
+  r is a reader
+  w is a writer
+  """
+
+  strr = r.read()
+  assert len(strr)>0
+  tree = ET.parse(StringIO(strr)) #importing the XML
+  root = tree.getroot()
+  WCDB1_print(w, ET.tostring(root)) #sending tree to the printer
+
